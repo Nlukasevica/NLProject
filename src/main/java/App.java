42127cb5a32786bfe41be9d771accfba50e28301
Nlukasevica <Lukasevica78@gmail.com>
2022-10-17 14:18:03 -0500

    One  more time Commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main (String[]args){
         System.out.println("Hello World");
+        System.out.println ("I worked hard and will learn more new things");
     }
 }
